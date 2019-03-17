e9960689f848d80eca943ef04052650b95c208bb
Jordan Brown <ojay8485@gmail.com>
2019-03-17 22:55:06 +0000

    Fixing

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 application = Flask(__name__)
 application.config['SECRET_KEY'] = "change this to be a more random key"
 application.config['UPLOAD_FOLDER'] = 'app/static/profiles'
-application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://zuhhronmafyzwe:7f46727df561c73a7e6d29d8ebb8b6efabf3e6cf70570d374fe5c4bde8450ac1@ec2-50-19-109-120.compute-1.amazonaws.com:5432/d1v4jugmdblkhh"
+application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://udznciuarbcurz:c7a02c666749bbdf711ee95ce6aa50aaa9fa82d0f396ade149bf910c024981f3@ec2-50-19-109-120.compute-1.amazonaws.com:5432/d5rtn4iqg7sdn9"
 db = SQLAlchemy(application)
 Base = db.Model
 session = db.session
